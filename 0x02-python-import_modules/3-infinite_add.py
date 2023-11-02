e8428d0b633470f933f312e59999e15ee593b986
saminstein <saminstein@yahoo.com>
2023-11-02 14:30:29 +0100

    infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    """
+    prints the result of the addition of all arguments
+    """
+
+num_args = len(sys.argv)
+
+if num_args == 1:
+    print("0")
+elif num_args >= 3:
+    total = 0
+    for args in sys.argv[1:]:
+        total += int(args)
+
+    print(f"{total}")
