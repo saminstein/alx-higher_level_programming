7676f138a30e249ba949b96bc98b541e9863baf6
saminstein <saminstein@yahoo.com>
2023-11-02 14:38:03 +0100

    Build my own calculator#

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
+
+    num_args = len(argv)
+    if num_args != 4:
+        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+
+    operator = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    result = operators[operator](a, b)
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
