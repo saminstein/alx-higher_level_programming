b86b038c05e946ef3eadd3b7c176432d9dd8a809
saminstein <saminstein@yahoo.com>
2023-11-05 17:50:14 +0100

    Can you c me now

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+     result_str = ""
+
+     for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        result_str += char
+     
+     return result_str
