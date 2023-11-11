0a6a2977acbae82cb0d9fc4736dfb003058f8524
saminstein <saminstein@yahoo.com>
2023-11-11 14:12:10 +0100

    Search and Replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+def search_replace(my_list, search, replace):
+  new_list = []
+  
+  for elems in my_list:
+    if elems == search:
+      new_list.append(replace)
+    else:
+      new_list.append(elems)
+  
+  return new_list
