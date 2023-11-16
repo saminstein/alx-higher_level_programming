48454c74eb31df25583843174d6ac4fbc7edd848
saminstein <saminstein@yahoo.com>
2023-11-16 14:59:21 +0100

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+def best_score(a_dictionary):
+  if a_dictionary is None:
+    return None
+    
+  best_value = 0
+  best_key = None
+  for key, value in a_dictionary.items():
+    if value > best_value:
+      best_value = value
+      best_key = key
+  return best_key
