8949f031e6fe6b2462945b78c263b4f18cfa1c46
saminstein <saminstein@yahoo.com>
2023-11-16 15:03:24 +0100

    Weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+def weight_average(my_list=[]):
+  if my_list == 0:
+    return 0
+    
+  total_score = 0
+  total_weight = 0
+  
+  for elem in my_list:
+    total_score += elem[0] * elem[1]
+    total_weight += elem[1]
+    weight_v = total_score / total_weight
+  return weight_v
