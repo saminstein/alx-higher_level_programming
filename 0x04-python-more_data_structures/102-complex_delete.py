13925ade2a69f8e1f821ff5ab2d50b70e7f1b763
saminstein <saminstein@yahoo.com>
2023-11-16 15:09:27 +0100

    Delete by value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+def complex_delete(a_dictionary, value):
+    keys_to_del = a_dictionary.copy()
+    for key, val in keys_to_del.items():
+      if val == value:
+        del a_dictionary[key]
+  
+    return a_dictionary
