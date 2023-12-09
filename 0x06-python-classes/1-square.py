06e69f70531b19ca8d0f4aaf11bdb6dc216f4ccb
saminstein <saminstein@yahoo.com>
2023-12-09 16:00:52 +0100

    Size validation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+class Square:
+  def __init__(self, size):
+    """__init__
+    The __init__ method initializes the size value of the square
+    
+    args: 
+      size (int): The size of the square
+    """
+    self._size = size
