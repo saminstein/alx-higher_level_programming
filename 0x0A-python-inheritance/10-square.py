dc0dd4ac956de8aa056379ab964281a6b67f2bd7
saminstein <saminstein@yahoo.com>
2024-01-03 09:45:48 +0100

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A Subclass square module
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+  def __init__(self, size):
+    self.__size = size
+    
+    super().__init__(size, size)
+    #self.integer_validator("size", size)
+    
+  def area(self):
+    return self.__size ** 2
+    
+  def __str__(self):
+    return "[Rectangle] {}/{}".format(self.__size, self.__size)
