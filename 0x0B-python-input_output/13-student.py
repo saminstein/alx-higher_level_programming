2cf3f425b42fe85fc382a9f08adb7d669bdc5fd9
saminstein <saminstein@yahoo.com>
2024-01-11 11:47:28 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+student to disk and reload module
+"""
+
+class Student:
+  def __init__(self, first_name, last_name, age):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+    
+  def to_json(self, attrs=None):
+    if attrs is not None:
+       result_dict = {}
+       for attr in attrs:
+         if hasattr(self, attr):
+           result_dict[attr] = getattr(self, attr)
+         return result_dict
+    else:
+      return self.__dict__
+      
+  def reload_from_json(self, json):
+    """
+    replaces all attributes of the student instance
+    """
+    for attr in json:
+      self.__dict__[attr] = json[attr]
