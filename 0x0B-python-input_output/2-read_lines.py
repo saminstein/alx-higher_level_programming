3dcb5ca6dd5317e37ab5f6f175cf6c9dcdf9874b
saminstein <saminstein@yahoo.com>
2024-01-11 10:52:25 +0100

    Read n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def read_lines(filename="", nb_lines=0):
+  with open(filename, 'r', encoding='utf-8') as rf:
+    lines = rf.readlines()
+    number = len(lines)
+    if nb_lines <= 0 or nb_lines >= number:
+      for line in lines:
+        print(line, end='')
+    else:
+      for i in range(nb_lines):
+        print(lines[i], end='')
+    
