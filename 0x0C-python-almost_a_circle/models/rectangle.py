3c3b2d08f70e94e73a8374b4997f31388f338f79
saminstein <saminstein@yahoo.com>
2024-01-17 11:50:37 +0100

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,6 +105,11 @@ class Rectangle(Base):
     
     return f'[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}'
     
-  
+  def update(self, *args):
+    attr = ['id', 'width', 'height', 'x', 'y']
+    
+    for i, arg in enumerate(args):
+      if i < len(attr):
+        setattr(self, attr[i], arg)
     
   
