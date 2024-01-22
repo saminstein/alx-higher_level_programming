0f42bcd48d57eaf6a3a95c3994e090869b071c92
saminstein <saminstein@yahoo.com>
2024-01-22 10:15:00 +0100

    Square size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+  def __init__(self, size, x=0, y=0, id=None):
+    ''' class constructor[super] that calls the constructor in the parent class[rectangle] and automatically assigns sets <width> & <height> to <size> '''
+    
+    super().__init__(size, size, x, y, id)
+    
+  @property
+  def size(self):
+    return self.width
+    
+  @size.setter
+  def size(self, value):
+    self.width = value
+    self.height = value
+    
+  def __str__(self):
+    return f'[Square] ({self.id}), {self.x}/{self.y} - {self.size}'
