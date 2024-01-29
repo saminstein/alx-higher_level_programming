4e0af4ba1e776b38efa411908d47b32789de91d7
saminstein <saminstein@yahoo.com>
2024-01-29 20:26:07 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -23,8 +23,8 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_subclass(self):
         ''' tests if Rectangle class inherits
         from Base Class '''
-        
-        self.assertIsTrue(issubclass(Rectangle, Base))
+
+        self.assertTrue(issubclass(Rectangle, Base))
 
     def test_rectangle_param(self):
         ''' tests parameters for rectangle class '''
@@ -48,6 +48,77 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r3.height, 2)
         self.assertEqual(r3.x, 0)
         self.assertEqual(r3.y, 0)
-        
-        with self.assertEqual(TypeError):
+
+        with self.assertRaises(TypeError):
             r4 = Rectangle()
+
+    def test_type_params(self):
+        '''
+        tests with different types of
+        parameters for the rectangle class:
+        'width, 'height', 'x', 'y'
+        '''
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(5.1, 3, 7, 8)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle('', 3, 7, 8)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, 3, 7, 8)
+
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-41567, 3, 7, 8)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(3, 4.2, 7, 8)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, '', 8, 9)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, None, 8, 9)
+
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(7, -21113, 8, 9)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, 8, 6.3, 9)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, 8, '', 9)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, 8, None, 9)
+
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(7, 8, -11678, 9)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, 8, 9, 8.9)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, 8, 9, '')
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(7, 8, 9, None)
+
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(7, 8, 9, -8)
+
+    def test_area(self):
+        ''' tests for width and height '''
+
+        r4 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r4.area(), 20)
+
+    def test_str_(self):
+        ''' test the str method '''
+
+        r1 = Rectangle(3, 2, 1, 1, 4)
+        self.assertEqual(str(r1), '[Rectangle] (4)         1/1 - 3/2')
+
+    def test_invalid_arg(self):
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(3, 2, 1, 1, 4, 10)
