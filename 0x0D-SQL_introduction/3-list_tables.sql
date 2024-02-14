7e12a256c1678e97328af0016a3a8a29bda14333
saminstein <saminstein@yahoo.com>
2024-02-14 13:47:41 +0100

    fix

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
---- script that lists all the tables of a database in your MySQL server.
----  database name will be passed as argument of mysql command
+-- script that lists all the tables of a database in your MySQL server.
+--  database name will be passed as argument of mysql command
 
 SHOW TABLES;
