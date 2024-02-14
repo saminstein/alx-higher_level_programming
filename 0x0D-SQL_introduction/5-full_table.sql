b0331596bc01ae3a9264743ff4c56604414d69e0
saminstein <saminstein@yahoo.com>
2024-02-14 15:00:07 +0100

    changes

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
---- script that prints the full description of the table from the database hbtn_0c_0 in your MySQL server.
+-- script that prints the full description of the table from the database hbtn_0c_0 in your MySQL server.
 
 SHOW CREATE TABLE first_table;
