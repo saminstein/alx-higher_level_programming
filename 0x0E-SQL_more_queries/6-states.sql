7a36b7ddb6bb53eb096dbd13019c76b768bd490a
saminstein <saminstein@yahoo.com>
2024-02-23 12:16:34 +0100

    ..

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,6 +4,8 @@
 -- name VARCHAR(256) can’t be null
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
   id INT AUTO_INCREMENT PRIMARY KEY, 
   name VARCHAR(256) NOT NULL
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,7 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 -- 
 
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
