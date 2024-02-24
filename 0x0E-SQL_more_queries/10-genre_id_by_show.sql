855b81aa9ea6d404f3ded30330c70bb050beb663
saminstein <saminstein@yahoo.com>
2024-02-24 11:25:33 +0100

    ..

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,6 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-CREATE DATABASE hbtn_0d_tvshows;
 
 SELECT tv_shows.title,tv_show_genres.genre_id
 FROM tv_shows JOIN tv_show_genres ON tv_shows.id  = tv_show_genres.show_id 
