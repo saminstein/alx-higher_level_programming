e8574ab74e4669a6a74e4c2c84b76552450b5e11
saminstein <saminstein@yahoo.com>
2024-02-27 10:53:22 +0100

    Add states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,12 @@ if __name__ == '__main__':
     the database
     '''
 
-    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+      host='localhost',
+      user=argv[1],
+      port=3306,
+      passwd=argv[2],
+      db=argv[3])
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
