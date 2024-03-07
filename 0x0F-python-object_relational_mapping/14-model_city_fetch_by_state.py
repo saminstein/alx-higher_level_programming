3ad66166738b5f45994a98ee193fc1a40c7b936d
saminstein <saminstein@yahoo.com>
2024-03-07 19:37:09 +0100

    ..

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from sys import argv
-from model_state import Base, State
+from model_state import Base, State, City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
