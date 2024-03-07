d67a27d3fc1b1dacf14c717a8f041d17e170827a
saminstein <saminstein@yahoo.com>
2024-03-07 14:30:00 +0100

    ..

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+script that deletes all State objects 
+with a name containing the letter 'a' from the
+database
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """
+    Updates a State object on the database
+    """
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    
+    state_to_delete = session.query(State).filter(State.name.like(%a%)).first()
+    state_to_update.name = 'New Mexico'
+    
+    for state in state_to_delete:
+        session.delete(state)
+        
+    session.commit()
+    
+    session.close()
