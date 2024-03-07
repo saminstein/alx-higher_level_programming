7a4cb4f5b26621fc43616224f0a157b4d54d94c4
saminstein <saminstein@yahoo.com>
2024-03-07 12:35:30 +0100

    ..

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,4 @@
-_#!/usr/bin/python3
+#!/usr/bin/python3
 
 from sys import argv
 from model_state import Base, State
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
+  
     db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
@@ -13,8 +14,6 @@ if __name__ == '__main__':
     session = Session()
     
     state_to_update = session.query(State).filter_by(id=2).first()
-    
-    #if state_to_update:
     state_to_update.name = 'New Mexico'
           
     session.commit()
