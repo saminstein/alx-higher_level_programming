746d82328127de96fd0e91b05cabbbd7dd62abaa
Saminstein <saminstein@yahoo.com>
2024-03-12 14:34:57 -0700

    changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(0, 9):
     for j in range(i+1, 10):
-        if i == 8 and j == 9:
+        if (i == 8 and j == 9):
             print("89")
             break
         print(f"{i}{j}, ", end="")
