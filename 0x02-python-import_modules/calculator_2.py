f193806abe5db97e51dcf751320ae6ed783d1000
saminstein <saminstein@yahoo.com>
2024-03-15 21:28:35 +0100

    ..

---
+++ 0x02-python-import_modules/calculator_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3 
+ def add(a, b): 
+     return a + b 
+  
+  
+ def sub(a, b): 
+     return a - b 
+  
+  
+ def mul(a, b): 
+     return a * b 
+  
+  
+ def div(a, b): 
+     if b == 0: 
+         return "Cannot Divide By Zero" 
+     return a / b
