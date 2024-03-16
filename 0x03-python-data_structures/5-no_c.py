e8cfac7aa0b67a6e62b0c09b8f655afc53d3949c
saminstein <saminstein@yahoo.com>
2024-03-16 18:24:26 +0100

    ..

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,6 @@ def no_c(my_string):
 
     for char in my_string:
         if char != 'c' and char != 'C':
-            continue
-        result_str += char
+            result_str += char
 
     return result_str
