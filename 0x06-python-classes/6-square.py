7a547b50942c7dab00cff81cd61d68eb872410bf
Saminstein <saminstein@yahoo.com>
2024-04-02 15:24:58 -0700

    ..

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
+        """ initializes a new square
+
+        args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """ gets size value """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ sets size value """
         self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -19,10 +31,12 @@ class Square:
 
     @property
     def position(self):
+        """ gets the position """
         return self.__position
 
     @position.setter
     def position(self, value):
+        """ sets the current position """
         self.__position = value
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integer")
@@ -34,9 +48,11 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integer")
 
     def area(self):
+        """ function returns current area of the square """
         return (self.__size) ** 2
 
     def my_print(self):
+        """ prints the square with # character """
         if self.__size == 0:
             print()
         else:
