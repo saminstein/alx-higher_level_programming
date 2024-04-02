40945614bec1e2b0cae7d03bd753c15df72520ff
Saminstein <saminstein@yahoo.com>
2024-04-02 14:56:50 -0700

    ..

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
 
 class Square:
+    """ class Square that defines a square"""
+
     def __init__(self, size=0):
+        """ initializes a square
+
+        args:
+            value (int): size of the square.
+        """
         self.__size = size
 
     @property
     def size(self):
+        """ initializes private square
+
+        Returns:
+               private size
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        The size setter method update the size value of the square.
+
+        args:
+            value (int): size of the square.
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -18,4 +38,7 @@ class Square:
             self.__size = value
 
     def area(self):
+        """
+        Returns the current square area
+        """
         return self.__size ** 2
