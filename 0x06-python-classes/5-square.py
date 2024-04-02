dfcf2796b2feb3fe851271be68ad193ec95f86b2
Saminstein <saminstein@yahoo.com>
2024-04-02 15:12:27 -0700

    ..

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
 
 class Square:
+    """ class Square that defines a square"""
+
     def __init__(self, size=0):
+        """ initializes a square
+
+        args:
+            value (int): size of the square.
+        """
         self.__size = size
 
     @property
     def size(self):
+        """ sets private size
+
+        Returns:
+               private size
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ The size setter method update the size value of the square.
+
+        args:
+            value (int): size of the square.
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -18,10 +37,15 @@ class Square:
             self.__size = value
 
     def area(self):
+        """ returns the area
+
+        Returns:
+               the area
+        """
         return (self.__size) ** 2
 
     def my_print(self):
-        """prints a square"""
+        """prints #"""
 
         if self.__size == 0:
             print("")
