d3d1231bd247ef0d39019243090a9c835054f919
saminstein <saminstein@yahoo.com>
2024-04-09 03:02:27 +0100

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """ class rectangle """
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height """
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """ width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ width setter """
         self.__width = value
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -22,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """ height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ height setter """
         self.__height = value
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -33,16 +39,19 @@ class Rectangle:
             raise ValueError("width must be >= 0")
 
     def area(self):
+        """ return rectangle area """
         result = self.__width * self.__height
         return result
 
     def perimeter(self):
+        """ return rectangle parameter """
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__width + self.__height)
 
     def draw_rectangle(self):
+        """ draw the rectangle with their sizes """
         empty_str = ""
         width = self.__width
         height = self.__height
