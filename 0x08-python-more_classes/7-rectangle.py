f5247a0e34b75aafaf89b4b21c010bf3bc822090
saminstein <saminstein@yahoo.com>
2024-04-09 19:28:26 +0100

    change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 
+"""
+empty class rectangle that defines a rectangle
+"""
+
 class Rectangle:
+    """ class rectangle """
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height """
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """
+        width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        width setter
+        """
         self.__width = value
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -23,10 +35,16 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        height setter
+        """
         self.__height = value
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -34,16 +52,25 @@ class Rectangle:
             raise ValueError("width must be >= 0")
 
     def area(self):
+        """
+        return the area of a rectangle
+        """
         result = self.__width * self.__height
         return result
 
     def perimeter(self):
+        """
+        return the perimeter of a rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__width + self.__height)
 
     def draw_rectangle(self):
+        """
+        prints #
+        """
         empty_str = ""
         width = self.__width
         height = self.__height
@@ -61,6 +88,9 @@ class Rectangle:
         return empty_str
 
     def __str__(self):
+        """
+        return a string representation of an instance
+        """
         return self.draw_rectangle()
 
     def __repr__(self):
