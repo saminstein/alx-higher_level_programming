932cc18dc59e85cc413f5617b02b8ab0477fb69d
Saminstein <saminstein@yahoo.com>
2024-04-08 17:53:36 -0700

    ..

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,13 +17,13 @@ class Rectangle:
             width: The width of  the rectangle
             height: The height of the rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """ 
-        returns the width of the rctangle
+        returns the width of the rectangle
         """
         return self.__width
 
@@ -32,12 +32,12 @@ class Rectangle:
         """
         width setter
         """
-        self.__width = value
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -51,8 +51,9 @@ class Rectangle:
         """
         height setter
         """
-        self.__height = value
+
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
