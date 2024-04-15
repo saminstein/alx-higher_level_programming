e7e0e5828ebffb872e0f25d058d4fcdd861766dd
Saminstein <saminstein@yahoo.com>
2024-04-14 19:46:49 -0700

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+function that returns the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """
     function that returns the list of
