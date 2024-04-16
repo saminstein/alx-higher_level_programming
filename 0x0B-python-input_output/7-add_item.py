8cfd8c13e5dfcd38c17d0b49386343f942a2cbf3
Saminstein <saminstein@yahoo.com>
2024-04-16 13:18:16 -0700

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
 from os import path
 from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 emp_list = []
 num_arg = len(argv)
