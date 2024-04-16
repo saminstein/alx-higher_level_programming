7ccc36d9e10bbd1b99aad346bf3b42e33819518f
Saminstein <saminstein@yahoo.com>
2024-04-16 14:16:45 -0700

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,18 @@
 
 
 class Student:
+    """
+    class that creates student instances
+    """
+
     def __init__(self, first_name, last_name, age):
+        """ initializes """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        returns dictionary description
+        """
         return self.__dict__
