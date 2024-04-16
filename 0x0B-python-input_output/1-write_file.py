4c5896dcee443f4f20f929886cf7175583be9436
saminstein <saminstein@yahoo.com>
2024-04-16 17:33:23 +0100

    ..

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write to a file
+"""
+
+def write_file(filename="", text=""):
+    """
+    function that writes to a text file
+
+    Args:
+        filename: filename
+        text: text to write
+    """
+    with open(filename, mode='w', encoding='utf-8') as wf:
+        return wf.write(text)
--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def write_file(filename="", text=""):
-    with open(filename, mode='w', encoding='utf-8') as wf:
-        wf.write(text)
-        number = len(text)
-        return number
