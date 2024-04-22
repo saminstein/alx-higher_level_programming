edd517e94b27672f19fe01a717281db227ad6057
Saminstein <saminstein@yahoo.com>
2024-04-22 15:38:13 -0700

    chnages

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -138,6 +138,3 @@ class TestSquare(unittest.TestCase):
         s4 = Square(1, 1)
         s4_dictionary = s4.to_dictionary()
         self.assertIsInstance(s4_dictionary, dict)
-
-if __name__ == "__main__":
-    unittest.main()
