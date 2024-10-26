eb3818027a5b054a478750495fd0184e6ffd4339
saminstein <saminstein@yahoo.com>
2024-10-26 21:04:57 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const argv = process.argv.length - 2;
+const argv = process.argv.length;
 
-if (argv === 0) {
+if (argv === 2) {
   console.log('No argument');
-} else if (argv === 1) {
+} else if (argv === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
