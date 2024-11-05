356d93086565835575ac96c0c758dfd15b2a859b
saminstein <saminstein@yahoo.com>
2024-11-05 13:44:19 +0100

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const number = process.argv[2];
+const size = parseInt(number);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
